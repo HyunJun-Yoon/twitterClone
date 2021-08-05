647a4f1b0b5a103010b130f677565945345d9d9d
Hyunjun Yoon <yhj940719@gmail.com>
2021-08-05 14:42:42 -0400

    added edit tweet

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import { dbService } from 'fBase';
+import React, { useState } from 'react';
 
-const Tweet = ({ tweetObj, isOwner }) => (
-  <div>
-    <h4>{tweetObj.text}</h4>
-    {isOwner && (
-      <>
-        <button>Delete Tweet</button>
-        <button>Edit Tweet</button>
-      </>
-    )}
-  </div>
-);
+const Tweet = ({ tweetObj, isOwner }) => {
+  const [editing, setEditing] = useState(false);
+  const [newTweet, setNewTweet] = useState(tweetObj.text);
+  const onDeleteClick = async () => {
+    const ok = window.confirm('Are you sure you want to delete this tweet?');
+    if (ok) {
+      await dbService.doc(`tweets/${tweetObj.id}`).delete();
+    }
+  };
+
+  const toggleEditing = () => setEditing(prev => !prev);
+
+  return (
+    <div>
+      {editing ? (
+        <>
+          <form>
+            <input value={newTweet} required />
+          </form>
+          <button onClick={toggleEditing}>Cancel</button>
+        </>
+      ) : (
+        <>
+          {' '}
+          <h4>{tweetObj.text}</h4>
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>Delete Tweet</button>
+              <button onClick={toggleEditing}>Edit Tweet</button>
+            </>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Tweet;
